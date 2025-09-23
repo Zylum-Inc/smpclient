ca20f2e8e7660b721c6fd6a7267a417f0bc4b6a0
Sidd <sgupta.ctr@muellerwp.com>
2025-09-23 15:11:35 -0400

    Made the chances of a nudge being sent down even more remote... (I suspect they might not been needed at all)

--- smpclient/transport/chirpstack_fuota.py
+++ smpclient/transport/chirpstack_fuota.py
@@ -503,7 +503,7 @@ class SMPChirpstackFuotaTransport(SMPTransport):
 
     def _should_send_nudge(self, no_uplink_count: int) -> bool:
         """Determine if we should send a nudge to trigger a response."""
-        return no_uplink_count >= 4
+        return no_uplink_count >= 10
 
     async def _send_nudge(self, dev_eui: str) -> None:
         """Send a random unicast message to nudge the device for a response."""
